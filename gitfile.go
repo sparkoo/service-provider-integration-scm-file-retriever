a3225c81a68df4b018539970e9a95c312ca5ea19
Max Shaposhnik <mshaposh@redhat.com>
2022-01-04 13:39:24 +0200

    Fixup lint
    Signed-off-by: Max Shaposhnik <mshaposh@redhat.com>

--- gitfile.go
+++ gitfile.go
@@ -7,7 +7,7 @@ import (
 	"io"
 )
 
-//nolint:unused
+//nolint
 func getFileContents(ctx context.Context, repoUrl, filepath, ref string, callback func(url string)) (io.ReadCloser, error) {
 
 	fileUrl, err := Detect(repoUrl, filepath, ref)
