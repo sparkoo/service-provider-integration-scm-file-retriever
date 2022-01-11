302b5d812f0409aea7959a2353aec8dfa4518a8f
Max Shaposhnik <mshaposh@redhat.com>
2022-01-11 10:26:49 +0200

    Add some comments
    Signed-off-by: Max Shaposhnik <mshaposh@redhat.com>

--- gitfile/tokenfetcher.go
+++ gitfile/tokenfetcher.go
@@ -1,9 +1,12 @@
 package gitfile
 
+// HeaderStruct is the simple struct to carry authentication string from different suppliers
 type HeaderStruct struct {
 	Authorization string `json:"Authorization"`
 }
 
+// TokenFetcher is the interface for the authentication token suppliers which are provides tokens as a HeaderStruct
+// instances
 type TokenFetcher interface {
 	BuildHeader(repoUrl string) HeaderStruct
 }
--- gitfile/tokenfetcher_env_var.go
+++ gitfile/tokenfetcher_env_var.go
@@ -2,6 +2,7 @@ package gitfile
 
 import "os"
 
+// EnvVarTokenFetcher token fetcher implementation that looks for token in the specific ENV variable.
 type EnvVarTokenFetcher struct{}
 
 func (s *EnvVarTokenFetcher) BuildHeader(repoUrl string) HeaderStruct {
